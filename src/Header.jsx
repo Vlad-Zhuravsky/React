884c2073b8a2aaf9c71b57fe16c77c690e178884
Dal-Ress <zhuravsky.vlad@gmail.com>
2020-12-21 14:55:08 +0300

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return <div>
+      <ul>
+        <li>CSS</li>
+        <li>HTML</li>
+        <li>JS</li>
+        <li>REACT</li>
+      </ul>
+    </div>
+  }
+
+  export default Header;
